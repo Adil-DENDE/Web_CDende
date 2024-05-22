854b3794a36ed64a3d32cccd3df619bb8185869f
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-22 22:43:09 +0200

    Spinner toegevoegd op de home page coming soon..

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,27 @@
 <template>
   <main>
-    <nav>
-      <NavigationBar/> 
-    </nav>
+    <NavigationBar />
+    <main class="pageContainer">
+      <div class="container">
+        <div class="d-flex justify-content-center mt-5">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </div>
+      <h5 class="text-center">J'ai just pas d'inspi coming soon..?</h5>
+    </main>
+
   </main>
 </template>
 
 <script setup>
 import NavigationBar from '@/components/NavigationBar.vue';
 </script>
+
+<style scoped>
+.pageContainer {
+  height: 100vh;
+  background-color: rgb(255, 255, 255);
+}
+</style>
