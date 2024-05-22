635dd673ab8c717f43b7b1cf2736522ea3ac3d7a
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-22 02:55:38 +0200

    Update login page links to use consistent paths

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -39,7 +39,7 @@
 
 
                     <div class="d-flex justify-content-between">
-                        <a class="btn btn-link p-lg-0 font-monospace txtAccount" href="/register.vue">Geen account?</a>
+                        <a class="btn btn-link p-lg-0 font-monospace txtAccount" href="/register">Geen account?</a>
                         <a class="btn btn-link p-lg-0 font-monospace txtPwd" href="/recover">Paswoord vergeten?</a>
                     </div>
 
