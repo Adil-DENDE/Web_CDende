08c06a390165d02db6f15f28fe1ac27270187fd9
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-06-01 04:24:57 +0200

    Car page details toegevoegd

--- src/views/Car/CarDetailsPage.vue
+++ src/views/Car/CarDetailsPage.vue
@@ -1,8 +1,15 @@
-<script setup >
+<script setup>
 import axios from 'axios';
-import { useRoute } from 'vue-router';
-
+import { useRoute, useRouter } from 'vue-router';
 const route = useRoute();
+const router = useRouter();
+
+// Back button //
+function backButton() {
+    router.go(-1);
+}
+
+
 axios.get('https://freetestapi.com/api/v1/cars/' + route.params.id)
     .then((response) => {
         console.log(response.data);
@@ -15,5 +22,20 @@ axios.get('https://freetestapi.com/api/v1/cars/' + route.params.id)
 
 
 <template>
-    <h1>VOITURE ID : {{ route.params.id }} </h1>
+    <input type="button" class="btn btn-secondary" value="Retour" @click="backButton">
+    <div class="container bg-secondary rounded-2">
+        <h1 class="text-center">Car : {{ route.params.id }}</h1>
+        <div class="row">
+            <div class="col-6">
+                <img src="https://via.placeholder.com/300" alt="Car image" class="img-fluid rounded-3">
+            </div>
+            <div class="col-6">
+                <p>Car make: pipi </p>
+                <p>Car model: caca</p>
+                <p>Car year: zizi</p>
+                <p>Car price: zezette</p>
+                <p>Car description:  zoulette</p>
+            </div>
+        </div>
+    </div>
 </template>
